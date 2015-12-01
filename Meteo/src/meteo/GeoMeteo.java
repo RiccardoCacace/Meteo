6d02236651ad93c6eee5937a40471e1cc37ec82a
Riccardo Cacace <riccardo.cacace96@gmail.com>
2015-12-01 19:00:09 +0100

    Proxy Removed

--- Meteo/src/meteo/GeoMeteo.java
+++ Meteo/src/meteo/GeoMeteo.java
@@ -39,7 +39,6 @@ public class GeoMeteo {
      */
     public static void main(String[] args) {
         
-        try {
             GeoMeteo geo = new GeoMeteo();
             String indirizzo = geo.getUserInput();
             //System.out.println(indirizzo);
@@ -58,58 +57,8 @@ public class GeoMeteo {
             else {
                 System.out.println("Impossibile trovare l'indirizzo.");
             }
-            System.setProperty("proxySet", "true");
-            System.setProperty("http.proxyHost", "192.168.0.1");
-            System.setProperty("http.proxyPort", "8080");
-            Authenticator.setDefault(new Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    
-                    return new PasswordAuthentication("NOMEUTENTE","PASSWORD".toCharArray());
-                }
-            });
-            
-            URL url = new URL("http://www.google.com/");
-            URLConnection con = url.openConnection();
-            
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    con.getInputStream()));
-
-// Read it ...
-            String inputLine;
-            while ((inputLine = in.readLine()) != null)
-                System.out.println(inputLine);
-            
-            in.close();
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(GeoMeteo.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(GeoMeteo.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    
     }
 
-    private static class ProxyAuthenticator extends Authenticator {
-
-        public ProxyAuthenticator(String userName, String password) {
-        this.userName = userName;
-        this.password = password;
-    }
-
-
-
-    private String userName, password;
-
-    protected PasswordAuthentication getPasswordAuthentication() {
-        return new PasswordAuthentication(userName, password.toCharArray());
-    }
-
-    
-
-    }
-    
-    
-    
     
     
     public GeoMeteo() {
